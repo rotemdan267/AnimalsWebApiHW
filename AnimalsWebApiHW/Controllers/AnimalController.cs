03e52c2ef21c63c92d67576cd0d0632507f6a28d
rotemdan267 <rotemdan267@gmail.com>
2022-06-02 02:23:51 +0300

    Add project files.

---
+++ AnimalsWebApiHW/Controllers/AnimalController.cs
@@ -0,0 +1,69 @@
+﻿using AnimalsWebApiHW.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace AnimalsWebApiHW.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnimalController : ControllerBase
+    {
+        public List<Animal> Animals { get; set; }
+
+        public AnimalController()
+        {
+            Animals = new List<Animal>()
+            {
+                new Animal() { Name = "Dog", Age = 5},
+                new Animal() { Name = "Cat", Age = 2},
+                new Animal() { Name = "Giraffe", Age = 8},
+                new Animal() { Name = "Horse", Age = 4},
+                new Animal() { Name = "Fish", Age = 1}
+            };
+        }
+
+        [Route("{name}")]
+        public IActionResult GetAnimalByName(string name)
+        {
+            if (name.ToLower() == "giraffe")
+            {
+                return LocalRedirect("~/api/animal/smile");
+            }
+
+            foreach (var animal in Animals)
+            {
+                if (animal.Name.ToLower() == name.ToLower())
+                {
+                    return Ok(animal);
+                }
+            }
+            return NotFound();
+        }
+
+        [Route("smile")]
+        public IActionResult Smile()
+        {
+            return Ok("             :)\n\n\n         :)\n\n\n                   :)");
+        }
+
+
+        [Route("special/{name}")]
+        public ActionResult<Animal> GetAnimalByName2(string name)
+        {
+            if (name.ToLower() == "giraffe")
+            {
+                return LocalRedirect("~/api/animal/smile");
+            }
+
+            foreach (var animal in Animals)
+            {
+                if (animal.Name.ToLower() == name.ToLower())
+                {
+                    return animal;
+                }
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ AnimalsWebApiHW/Models/Animal.cs
@@ -0,0 +1,8 @@
+﻿namespace AnimalsWebApiHW.Models
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
